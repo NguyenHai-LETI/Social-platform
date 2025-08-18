d7a355998be20bd3e9eb3255933ba6b38f7684c4
NguyenHai-LETI <nguyenhaipc98@gmail.com>
2025-08-18 16:30:06 +0700

    debug tool-bar added

--- backend/social_app/social_app/settings/develop.py
+++ backend/social_app/social_app/settings/develop.py
@@ -6,6 +6,20 @@ environ.Env.read_env(os.path.join(BASE_DIR.parent.parent, "envs/develop.env"))
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',  # Phải trước middleware khác
+] + MIDDLEWARE
+
+# Chỉ hiện toolbar cho localhost
+INTERNAL_IPS = [
+    '127.0.0.1',
+    'localhost',
+]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
